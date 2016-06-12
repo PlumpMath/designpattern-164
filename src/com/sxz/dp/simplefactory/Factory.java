ebce6778dc84d80a50f1323572dbd91cb1906c78
David Shen <google_1986@163.com>
2016-06-12 14:59:43 +0800

    code submit

---
+++ src/com/sxz/dp/simplefactory/Factory.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.sxz.dp.simplefactory;
+
+/**
+ * @author BG243085
+ *
+ */
+public class Factory {
+	public static ICar produce(String carType){
+		ICar car = null;
+		if("benz".equalsIgnoreCase(carType)){
+			car=new Benz();
+		}else if("bmw".equalsIgnoreCase(carType)){
+			car = new BMW();
+		}else if("geely".equalsIgnoreCase(carType)){
+			car=new Geely();
+		}else{
+			throw new RuntimeException("该品牌车辆，本生产类不支持生产");
+		}
+		return car;
+			
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ICar car=Factory.produce("1bmw");
+		car.run();
+
+	}
+
+}
